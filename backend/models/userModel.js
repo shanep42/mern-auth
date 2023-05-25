cc199750572aa4df7028917d268b6893df59ab58
Shane <shanep42@gmail.com>
2023-05-24 19:59:36 -0500

    Initial commit

---
+++ backend/models/userModel.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+import bcrypt from 'bcryptjs';
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+}, {
+    timestamps: true
+});
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
